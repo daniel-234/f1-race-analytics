033b44e9d3b80506704519a18219f20e3c185963
Daniele <danielerb.dev@gmail.com>
2026-02-12 11:00:40 +0100

    Get info as tuples from API

--- src/f1_race_analytics/__main__.py
+++ src/f1_race_analytics/__main__.py
@@ -5,12 +5,17 @@ from open_f1_data import fetch_meetings
 
 
 async def main():
-    races = await fetch_races()
-    meetings = await fetch_meetings()
-    print("RACES from Jolpica:\n")
-    print(races)
-    print("\n\nMeetings from OpenF1:")
-    print(meetings)
+    races_data = await fetch_races()
+    #meetings_data = await fetch_meetings()
+    
+    #grand_prix_names = [grand_prix_official for grand_prix_official in meetings_data.get("meeting_official_name")]
+    #print("\n\nMeetings from OpenF1:")
+    #print(grand_prix_names)
 
+    races = races_data.get('MRData').get('RaceTable').get('Races')
+    race_info = [(race.get('date'), race.get('raceName', {})) for race in races]
+    
+    print("RACES from Jolpica:\n")
+    print(race_info)
 
 asyncio.run(main())
