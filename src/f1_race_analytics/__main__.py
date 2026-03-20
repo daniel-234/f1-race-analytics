56abf1d39f543659a4cdeadd1a7f1f9bc8847125
Daniele <danielerb.dev@gmail.com>
2026-03-20 12:37:36 +0100

    Refactor: Delete unused main function
    Deleted the "main" method in __main__.py.
    Renamed the starting point for the app main server.
    Changed names to HTML link and page header.

--- src/f1_race_analytics/__main__.py
+++ src/f1_race_analytics/__main__.py
@@ -1,30 +1,7 @@
 import uvicorn
 
-from .app import YEAR
-from .database import (
-    clear_db_and_tables,
-    create_championship,
-    create_db_and_tables,
-    create_race_results,
-    create_races,
-)
-from .f1_data import fetch_constructor_driver_pairs, fetch_races, fetch_results_by_race
 
-CIRCUIT_ID = "monza"
-
-
-def main():
-    clear_db_and_tables()
-    create_db_and_tables()
-    races_data = fetch_races(YEAR)
-    create_races(YEAR, races_data)
-    constructor_driver_pairs = fetch_constructor_driver_pairs(YEAR)
-    create_championship(YEAR, constructor_driver_pairs)
-    result_data = fetch_results_by_race(YEAR, CIRCUIT_ID)
-    create_race_results(YEAR, result_data)
-
-
-def dev():
+def start():
     uvicorn.run("f1_race_analytics.app:app", reload=True)
 
 
@@ -34,7 +11,3 @@ def live():
         reload=True,
         port=8001,
     )
-
-
-if __name__ == "__main__":
-    main()
